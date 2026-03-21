551c12be15be95f7b29b563e33c50d17856488cb
khrynyshyn <kh.hrynyshyn@gmail.com>
2026-03-21 20:19:13 +0200

    MNFT-1 Add a new test with project creation

--- src/test/java/io/testomat/e2e_tests/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests/ProjectPageTests.java
@@ -29,6 +29,30 @@ public class ProjectPageTests {
 
         //wait for page to load
         $("h2").shouldHave(text("Manufacture Own"));
+    }
 
+    @Test
+    public void userCanCreateNewProject() {
+        open("https://app.testomat.io/");
+
+        //login
+        $("#content-desktop #user_email").setValue("grynyshyn2002@gmail.com");
+        $("#content-desktop #user_password").setValue("khrynyshyn_QA123");
+        $("#content-desktop #user_remember_me").click();
+        $("#content-desktop [name=\"commit\"]").click();
+        $(".common-flash-success").shouldBe(visible);
+
+        //create project
+        $("a.common-btn-primary[href='/projects/new']").click();
+        $("h2").shouldHave(text("New Project"));
+        $("#project_title").setValue("New Project");
+        $("[name='commit']").click();
+
+
+        //wait for project to create
+        sleep(10000);
+        $("#welcometotestomatio").shouldBe(visible);
+        $(".detail-view-header-wrapper .md-icon.md-icon-close").click();
+        $("h2").shouldHave(text("New Project"));
     }
 }
